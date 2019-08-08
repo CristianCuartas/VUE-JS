f69a2d4a24a64b9f38b6631207526cf8c5de4629
CristianCuartas <cristianhz1109@gmail.com>
2019-08-08 17:18:18 -0500

    Cambios => Home page base-prototipo1

--- rectangles-app/src/components/HomePage.js
+++ rectangles-app/src/components/HomePage.js
@@ -13,18 +13,48 @@ import {
     CardHeader,
     CardBody,
     CardFooter, 
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption,
+    CardSubtitle,
+    CardImg,
     Row, 
     Col
 } from 'reactstrap';
 import { animateScroll as scroll } from 'react-scroll';
-import cubeHome from './../assets/img/cubeHome.jpg';
+import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
+import facebook from './../assets/img/facebook.svg';
+import instagram from './../assets/img/instagram.svg';
+import cubeMail from './../assets/img/cubo.svg';
 import rectangleHome from './../assets/img/rectangle.gif';
-import './../assets/css/HomePage.css'
+import './../assets/css/HomePage.css';
+
+const items = [
+    {
+      id: 1,
+      altText: 'Slide 1',
+      caption: 'Slide 1'
+    },
+    {
+      id: 2,
+      altText: 'Slide 2',
+      caption: 'Slide 2'
+    },
+    {
+      id: 3,
+      altText: 'Slide 3',
+      caption: 'Slide 3'
+    }
+  ];
+
 class HomePage extends Component {
     constructor(props) {
         super(props);
         this.state={
-            collapsed: false
+            collapsed: false,
+            activeIndex: 0
         }
     }
     toggleNavBar = () => {
@@ -32,11 +62,59 @@ class HomePage extends Component {
             collapsed:!this.state.collapsed
         })
     }
-    onClickMoreDown = () => {
-        scroll.scrollMore(830);
+    onClickMoreDownPrototipos = () => {
+        scroll.scrollMore(351);
       }
-
+      onClickMoreDownZonas = () => {
+        scroll.scrollMore(1000);
+      }
+      onClickMoreDownInstalcion = () => {
+        scroll.scrollMore(1670);
+      }
+      onClickMoreDownTransporte = () => {
+        scroll.scrollToBottom();
+      }
+      onExiting = () => {
+        this.animating = true;
+      }
+    
+      onExited =() => {
+        this.animating = false;
+      }
+    
+      next = () => {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+        this.setState({ activeIndex: nextIndex });
+      }
+    
+      previous = () => {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+        this.setState({ activeIndex: nextIndex });
+      }
+    
+      goToIndex = (newIndex) => {
+        if (this.animating) return;
+        this.setState({ activeIndex: newIndex });
+      }
+    
     render(){
+        const { activeIndex } = this.state;
+
+        const slides = items.map((item) => {
+          return (
+            <CarouselItem
+              className="custom-tag"
+              tag="div"
+              key={item.id}
+              onExiting={this.onExiting}
+              onExited={this.onExited}
+            >
+              <CarouselCaption className="text-danger" captionText={item.caption} captionHeader={item.caption} />
+            </CarouselItem>
+          );
+        });
         return(
             <Fragment>
             <div className="container-fluid">
@@ -47,11 +125,11 @@ class HomePage extends Component {
                     <div className="navBar">
                         <Navbar color="faded" dark>
                             <NavbarBrand 
-                                style={{"color":"#FFFFFF"}} 
+                                style={{"color":"#FFFFFF", fontSize: "35px"}} 
                                 href="/" 
                                 className="mr-auto"
                             >
-                                RECTANGLES
+                                R E C T A N G L E S
                             </NavbarBrand>
                             <NavbarToggler 
                                 onClick={this.toggleNavBar} 
@@ -66,46 +144,40 @@ class HomePage extends Component {
                                 <Button
                                     className="btn btn-down" 
                                     color="link"
-                                    style={{"color":"#FFFFFF"}} 
-                                    onClick={this.onClickMoreDown}
+                                    style={{"color":"#FFFFFF", fontSize: "25px"}} 
+                                    onClick={this.onClickMoreDownPrototipos}
                                     >
-                                        Sub Menu 1
+                                        Prototipos
+                                </Button>
+                            </NavItem>
+                            <NavItem>
+                                <Button 
+                                    className="btn btn-down"
+                                    color="link"
+                                    style={{"color":"#FFFFFF", fontSize: "25px"}} 
+                                    onClick={this.onClickMoreDownZonas}
+                                    >
+                                        Zonas de vivienda
                                 </Button>
                             </NavItem>
                             <NavItem>
                                 <Button 
                                     className="btn btn-down" 
                                     color="link"
-                                    style={{"color":"#FFFFFF"}} 
-                                    onClick={this.onClickMoreDown}
+                                    style={{"color":"#FFFFFF", fontSize: "25px"}} 
+                                    onClick={this.onClickMoreDownInstalcion}
                                     >
-                                        Sub Menu 2
+                                        Instalación
                                 </Button>
                             </NavItem>
                             <NavItem>
-                                <Button 
+                                <Button
                                     className="btn btn-down" 
                                     color="link"
-                                    style={{"color":"#FFFFFF"}} 
-                                    onClick={this.onClickMoreDown}
+                                    style={{"color":"#FFFFFF", fontSize: "25px"}} 
+                                    onClick={this.onClickMoreDownTransporte}
                                     >
-                                        Sub Menu 3 
-                                </Button>
-                            </NavItem>
-                            <NavItem>
-                                <Button 
-                                    className="btn btn-down" 
-                                    color="link"
-                                    style={{"color":"#FFFFFF"}} 
-                                    onClick={this.onClickMoreDown}
-                                    >
-                                       Canales de comunicación
-                                       <br/>
-                                       <i className="fa fa-facebook-official" aria-hidden="true"></i>
-                                       &nbsp;
-                                       <i className="fa fa-instagram" aria-hidden="true"></i>
-                                       &nbsp;
-                                       <i className="fa fa-cubes" aria-hidden="true"></i>
+                                       Transporte
                                 </Button>
                             </NavItem>
                             </Nav>
@@ -117,17 +189,34 @@ class HomePage extends Component {
                 </div>
                 </div>
                 <div 
-                    className="container-fluid"
+                    className="container"
+                    style={{marginTop:"80px"}}
                 >
+                <h1 className="text-center title-prototipos">
+                Estos son nuestros prototipos</h1>
+                <br/>
                 <div className="row" style={{backgroundColor:"#F6F7C2"}}>
-                    <div className="col-sm-4"
+                
+                    <div className="col-md-4"
                     >
+                    
                     <Card>
                     <CardHeader className="text-center">Contenido Z</CardHeader>
                     <CardBody>
                     <CardTitle><img className="rectangleGif" src={rectangleHome} /></CardTitle>
-                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                    <Button outline color="warning" style={{marginLeft:"100px"}}>Go somewhere</Button>
+                    <CardText>
+                    With supporting text below as a natural lead-in to additional content.
+                    </CardText>
+                    <Button 
+                        outline 
+                        color="warning" 
+                        style={{
+                            display: "block",
+                            margin: "auto",
+                        }}
+                        >
+                        Go somewhere
+                    </Button>
                     </CardBody>
                     <CardFooter className="text-center">
                     <i className="fa fa-facebook-official" aria-hidden="true"></i>
@@ -138,15 +227,23 @@ class HomePage extends Component {
                     </CardFooter>
                 </Card>
                 </div>
-                <div className="col-sm-4"
+                <div className="col-md-4"
                     >
                     <Card>
                     <CardHeader className="text-center">Contenido Y</CardHeader>
                     <CardBody>
                     <CardTitle><img className="rectangleGif" src={rectangleHome} /></CardTitle>
-                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                    <Button outline color="warning" style={{marginLeft:"100px"}}>Go somewhere</Button>
-
+                    <CardText>
+                    With supporting text below as a natural lead-in to additional content.</CardText>
+                    <Button 
+                        outline 
+                        color="warning" 
+                        style={{
+                            display: "block",
+                            margin: "auto",
+                        }}>
+                        Go somewhere
+                        </Button>
                     </CardBody>
                     <CardFooter className="text-center">
                     <i className="fa fa-facebook-official" aria-hidden="true"></i>
@@ -157,15 +254,22 @@ class HomePage extends Component {
                     </CardFooter>
                 </Card>
                 </div>
-                <div className="col-sm-4"
+                <div className="col-md-4"
                 >
                     <Card>
                     <CardHeader className="text-center">Contenido X</CardHeader>
                     <CardBody>
                     <CardTitle><img className="rectangleGif" src={rectangleHome} /></CardTitle>
-                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                    <Button outline color="warning" style={{marginLeft:"100px"}}>Go somewhere</Button>
-
+                    <CardText>
+                    With supporting text below as a natural lead-in to additional content.
+                    </CardText>
+                    <Button outline color="warning" 
+                    style={{
+                        display: "block",
+                        margin: "auto",
+                    }}>
+                    Go somewhere
+                    </Button>
                     </CardBody>
                     <CardFooter className="text-center">
                     <i className="fa fa-facebook-official" aria-hidden="true"></i>
@@ -179,8 +283,79 @@ class HomePage extends Component {
                 <br/>
                 </div>
                 </div>
+                <div className="container" style={{marginTop:"100px"}}>
+                <div className="row">
+                <div className="col-md-12">
+                <h1 className="text-center title-map">Localiza tú zona de vivienda</h1>
+                <br/>
+                <div className="mapa">
+                <Map 
+                    google={this.props.google} 
+                    zoom={10}
+                    initialCenter={{
+                        lat: 4.6097100,
+                        lng: -74.0817500
+                    }}
+                >
+                <Marker onClick={this.onMarkerClick}
+                        name={'Current location'} />
+                </Map>
+                </div>
+                </div>
+                    </div>
+                </div>
+                <div className="container" style={{marginTop:"150px"}}>
+                <div className="row">
+                <div className="col-md-12">
+                <h1 className="text-center title-instalacion">
+                No te preocupes nosotros lo instalamos</h1>
+                <br/>
+                
+                    <Carousel
+                        activeIndex={activeIndex}
+                        next={this.next}
+                        previous={this.previous}
+                    >
+                    <CarouselIndicators 
+                        items={items} 
+                        activeIndex={activeIndex} 
+                        onClickHandler={this.goToIndex} 
+                        />
+                    {slides}
+                    <CarouselControl 
+                        direction="prev" 
+                        directionText="Previous" 
+                        onClickHandler={this.previous} 
+                        />
+                    <CarouselControl 
+                        direction="next" 
+                        directionText="Next" 
+                        onClickHandler={this.next} 
+                        />
+                    </Carousel>
+                </div>
+                    </div>
+                </div>
+                <div className="container" style={{marginTop:"100px", marginBottom:"500px"}}>
+                <div className="row">
+                <div className="col-md-12">
+                <h1 className="text-center title-transporte">
+                ¿Transporte? Lo resolvemos </h1>
+                <br/>
+                <center>
+                <img className="canalesComunicacion" src={facebook} />
+                &nbsp;
+                <img className="canalesComunicacion" src={instagram} />
+                &nbsp;
+                <img className="canalesComunicacion" src={cubeMail} />
+                </center>
+                </div>
+                    </div>
+                </div>
                 </Fragment>
         );
     }
 }
-export default HomePage;
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyAuOMCNsR9Pm4O3z9bEeDGC7FaQ_H921oQ")
+  })(HomePage)
